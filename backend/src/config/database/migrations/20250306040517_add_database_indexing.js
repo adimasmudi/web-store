37050a8c27c660484cd68d401ebb52d1a674487a
adimasmudi <adimasmudi@gmail.com>
2025-03-06 11:22:14 +0700

    refactor : add DESC in database indexing updated_at

---
+++ backend/src/config/database/migrations/20250306040517_add_database_indexing.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+  await knex.raw(`
+        CREATE INDEX idx_product_title ON products(title);
+        CREATE INDEX idx_product_category ON products(category);
+        CREATE INDEX idx_product_updated_at ON products(updated_at DESC);
+    `);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+  await knex.raw(`
+        DROP INDEX IF EXISTS idx_product_title;
+        DROP INDEX IF EXISTS idx_product_category;
+        DROP INDEX IF EXISTS idx_product_updated_at;
+    `);
+};
