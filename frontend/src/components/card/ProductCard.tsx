be2cf67a6686d4be0f85bfd6a87e420e40b4de7d
adimasmudi <adimasmudi@gmail.com>
2025-03-08 10:52:50 +0700

    feat : implement infinite render

--- frontend/src/components/card/ProductCard.tsx
+++ frontend/src/components/card/ProductCard.tsx
@@ -49,18 +49,10 @@ export const ProductCard = ({
             {description}
           </p>
         </div>
-        {/* {props.amountSold !== undefined && (
-          <div className="px-4 pt-2 flex flex-wrap justify-between">
-            <p className="text-slate-600">{props.amountSold} sold</p>
-            {props.distance !== undefined && props.distance !== 0 && (
-              <p className="text-slate-600">{props.distance.toFixed(2)} Km</p>
-            )}
-          </div>
-        )} */}
         <div className="p-4">
           <AppButton
             variant="primary"
-            // state={props.stock > 1 ? 'Active' : 'Disabled'}
+            state={stock > 1 ? 'Active' : 'Disabled'}
             className="rounded-md w-full py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-cyan-700 focus:shadow-none active:bg-cyan-700 hover:bg-cyan-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
             type="button"
             // onClick={(event) => {
--- frontend/src/features/user/home/components/productList/ProductList.tsx
+++ frontend/src/features/user/home/components/productList/ProductList.tsx
@@ -17,104 +17,127 @@ import {
   SelectValue
 } from '@/shadcn/components/ui/select';
 import { PRODUCT_CATEGORIES } from '@/types/constants';
-import { useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 export const ProductList = () => {
+  const [products, setProducts] = useState<ProductResData[]>([]);
   const [category, setCategory] = useState<string>('');
   const [search, setSearch] = useState<string>('');
   const [searchTemp, setSearchTemp] = useState<string>('');
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const [toggleRefetch, setToggleRefetch] = useState<boolean>(false);
+  const [hasMore, setHasMore] = useState(true);
 
   const { data, isLoading, error } = useFetch({
     fn: getProducts,
-    params: { search: search, category: category, page: currentPage },
-    refetch: toggleRefetch
+    params: { search, category, page: currentPage }
   });
+
+  const observer = useRef<IntersectionObserver | null>(null);
+  const lastProductRef = useCallback(
+    (node: HTMLDivElement) => {
+      if (isLoading || !hasMore) return;
+      if (observer.current) observer.current.disconnect();
+
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          setCurrentPage((prevPage) => prevPage + 1);
+        }
+      });
+
+      if (node) observer.current.observe(node);
+    },
+    [isLoading, hasMore]
+  );
+
+  useEffect(() => {
+    if (data?.data.items.length > 0) {
+      setProducts((prevState) => {
+        const newItems = data?.data.items.filter(
+          (item: ProductResData) =>
+            !prevState.some((prevItem) => prevItem.id === item.id)
+        );
+        return [...prevState, ...newItems];
+      });
+    } else {
+      setHasMore(false);
+    }
+  }, [data?.data.items]);
+
+  useEffect(() => {
+    setProducts([]);
+    setCurrentPage(1);
+    setHasMore(true);
+  }, [search, category]);
 
   return (
     <div className="mt-96 w-11/12">
-      {isLoading ? (
-        <Spinner />
-      ) : error ? (
-        <p>There is error when trying to display product data</p>
-      ) : (
-        <>
-          <h2>List of Products</h2>
-          <div className="flex flex-row justify-between">
-            <div>
-              <Select
-                onValueChange={(val) => {
-                  if (val === 'select_all') {
-                    setCategory('');
-                    return;
-                  }
-                  setCategory(val);
-                }}
-                defaultValue={category}
-              >
-                <SelectTrigger className="w-[180px]">
-                  <SelectValue placeholder="Select Category" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Category</SelectLabel>
-                    <SelectItem value="select_all">Display All</SelectItem>
-                    {PRODUCT_CATEGORIES.map((pc, idx) => {
-                      return (
-                        <SelectItem key={idx} value={pc}>
-                          {pc}
-                        </SelectItem>
-                      );
-                    })}
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-            <div className="flex flex-row gap-4">
-              <Input
-                placeholder="Enter a search keyword"
-                defaultValue={searchTemp}
-                onChange={(e) => setSearchTemp(e.target.value)}
-                onKeyDown={(e) => {
-                  if (e.key === 'Enter') {
-                    setCurrentPage(1);
-                    if (searchTemp.length >= 3) setSearch(searchTemp);
-                  }
-                }}
-              />
-              <AppButton
-                variant="primary"
-                state={searchTemp.length >= 3 ? 'Active' : 'Disabled'}
-                onClick={() => {
-                  setCurrentPage(1);
-                  if (searchTemp.length >= 3) setSearch(searchTemp);
-                }}
-              >
-                Search
-              </AppButton>
-            </div>
+      <h2>List of Products</h2>
+      <div className="flex flex-row justify-between">
+        <div>
+          <Select
+            onValueChange={(val) => {
+              setCategory(val === 'select_all' ? '' : val);
+            }}
+            defaultValue={category}
+          >
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Select Category" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Category</SelectLabel>
+                <SelectItem value="select_all">Display All</SelectItem>
+                {PRODUCT_CATEGORIES.map((pc, idx) => (
+                  <SelectItem key={idx} value={pc}>
+                    {pc}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex flex-row gap-4">
+          <Input
+            placeholder="Enter a search keyword"
+            defaultValue={searchTemp}
+            onChange={(e) => setSearchTemp(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter' && searchTemp.length >= 3) {
+                setSearch(searchTemp);
+              }
+            }}
+          />
+          <AppButton
+            variant="primary"
+            state={searchTemp.length >= 3 ? 'Active' : 'Disabled'}
+            onClick={() => {
+              if (searchTemp.length >= 3) setSearch(searchTemp);
+            }}
+          >
+            Search
+          </AppButton>
+        </div>
+      </div>
+      <div className="grid mx-5 xl:mx-0 grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+        {products.map((product: ProductResData, idx: number) => (
+          <div
+            key={product.id}
+            ref={idx === products.length - 1 ? lastProductRef : null}
+          >
+            <ProductCard
+              id={Number(product.id)}
+              title={product.title}
+              price={Number(product.price)}
+              description={product.description}
+              category={product.category}
+              image_path={product.image_path}
+              stock={Number(product.stock)}
+            />
           </div>
-          <div className="grid mx-5 xl:mx-0 grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-            {data?.data.items.length > 0
-              ? data?.data.items.map((dt: ProductResData, idx: number) => {
-                  return (
-                    <ProductCard
-                      key={idx}
-                      id={Number(dt.id)}
-                      title={dt.title}
-                      price={Number(dt.price)}
-                      description={dt.description}
-                      category={dt.category}
-                      image_path={dt.image_path}
-                      stock={Number(dt.stock)}
-                    />
-                  );
-                })
-              : 'There is no data to display'}
-          </div>
-        </>
-      )}
+        ))}
+        {isLoading && <Spinner />}
+        {error && <p className="text-red-500">Error loading products.</p>}
+      </div>
     </div>
   );
 };
