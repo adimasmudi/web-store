e453810b5dc2c2193ec9cccd8793bb2857c9b559
adimasmudi <adimasmudi@gmail.com>
2025-03-09 09:41:36 +0700

    refactor : bring back spinner to be small by default

--- frontend/src/components/spinner/Spinner.tsx
+++ frontend/src/components/spinner/Spinner.tsx
@@ -8,9 +8,7 @@ export const Spinner = ({ variant }: SpinnerProps) => {
     <div role="status" className="flex flex-row justify-center items-center">
       <svg
         aria-hidden="true"
-        className={`size-${
-          variant === 'large' ? '10' : '6'
-        } text-gray-200 animate-spin dark:text-gray-600 fill-teal-500`}
+        className={`size-6 text-gray-200 animate-spin dark:text-gray-600 fill-teal-500`}
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
